4ac3d4e8ce22c15c348ca2f983a607fb44f17140
= <=>
2022-05-27 13:38:02 +0300

    img slider dahil hali

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log(window.innerWidth)
+//console.log(window.innerWidth)
+
+const article = document.querySelectorAll(".slider-container article");
+const button = document.querySelectorAll(".slider-container button");
+
+let currentSlide = 0;
+
+setInterval(nextImage, 5000);
+
+function nextImage() {
+   article[currentSlide].classList.remove("active");
+  button[currentSlide].classList.remove("active");
+
+  currentSlide = (currentSlide + 1) % article.length;
+
+  article[currentSlide].classList.add("active");
+  button[currentSlide].classList.add("active");
+}
+
+
+
+
+
+
+const newNav = function (n) {
+  article.forEach((sect) => {
+    sect.classList.remove("active");
+  });
+  button.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  article[n].classList.add("active");
+  button[n].classList.add("active");
+};
+
+button.forEach((btn, i) => {
+  btn.addEventListener("mouseover", () => {
+    newNav(i);
+    currentSlide = i;
+  });
+});
+
+
+
